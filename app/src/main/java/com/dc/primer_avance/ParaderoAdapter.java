495382d137cff218cf9b0dd354d6ed68d1c6ab0d
FrancoDelCastilloUTP <109124389+FrancoDelCastilloUTP@users.noreply.github.com>
2023-09-15 18:43:35 -0500

    commit inicial

---
+++ app/src/main/java/com/dc/primer_avance/ParaderoAdapter.java
@@ -0,0 +1,11 @@
+package com.dc.primer_avance;
+
+import android.view.LayoutInflater;
+
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ParaderoAdapter {
+
+
+}
---
+++ app/src/main/java/com/dc/primer_avance/paradero.java
@@ -0,0 +1,19 @@
+package com.dc.primer_avance;
+
+public class paradero {
+
+    public String nombre;
+
+    public paradero(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
+
